9e315c6a15c27ac337ff5225a37c8426946f18ac
Sebastiano DAvolio <s.davolio.1297@vallauri.edu>
2020-09-29 11:32:39 +0200

    Auto Commit

--- 002/js/scripts.js
+++ 002/js/scripts.js
@@ -5,9 +5,10 @@ const input = document.getElementById('item');
 
 let itemsArray = [];
 
-localStorage.setItem('items', JSON.stringify(itemsArray));
+//localStorage.setItem('items', JSON.stringify(itemsArray)); NON RIPRISTINARE QUESTO E' ROTTO
 const data = JSON.parse(localStorage.getItem('items'));
 
+console.log(data);
 const liMaker = (text) => {
     const li = document.createElement('li');
     li.textContent = text;
@@ -17,6 +18,10 @@ const liMaker = (text) => {
   form.addEventListener('submit', function (e) {
     e.preventDefault();
   
+    itemsArray.push(input.value)
+    localStorage.setItem('items', JSON.stringify(itemsArray))
+
     liMaker(input.value);
     input.value = '';
+    e.preventDefault()
   })
